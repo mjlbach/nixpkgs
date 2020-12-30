0da72cb3e94e1efeb75b041b070c4c11f53f5e35
Michael Lingelbach <m.j.lbach@gmail.com>
2020-12-29 17:30:18 -0800

    sumneko-lua-language-server: init at 1.9.0

---
+++ pkgs/development/tools/sumneko-lua-language-server/default.nix
@@ -0,0 +1,50 @@
+{ stdenv, fetchFromGitHub, ninja, makeWrapper }:
+
+stdenv.mkDerivation rec {
+  pname = "sumneko-lua-language-server";
+  version = "1.9.0";
+
+  src = fetchFromGitHub {
+    owner = "sumneko";
+    repo = "lua-language-server";
+    rev = "${version}";
+    sha256 = "0d93ikxs0c5fm9k57xbzgphm90dnfqq3nz2bc3bqxi7gzyldb9z6";
+    fetchSubmodules = true;
+  };
+
+  nativeBuildInputs = [
+    ninja
+    makeWrapper
+  ];
+
+  patches = [
+    ./sumneko_home_expansion.patch
+  ];
+
+  buildPhase = ''
+    cd 3rd/luamake
+    ninja -f ninja/linux.ninja
+    cd ../..
+    ./3rd/luamake/luamake rebuild
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin $out/extras
+    cp -r ./{locale,meta,script,*.lua} $out/extras/
+    cp ./bin/Linux/{bee.so,lpeglabel.so} $out/extras
+    cp ./bin/Linux/lua-language-server $out/extras/.lua-language-server-unwrapped
+    makeWrapper $out/extras/.lua-language-server-unwrapped \
+      $out/bin/lua-language-server \
+      --add-flags "-E $out/extras/main.lua \
+      --logpath='~/.cache/sumneko_lua/log' \
+      --metapath='~/.cache/sumneko_lua/meta'"
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Lua Language Server coded by Lua ";
+    homepage = "https://github.com/sumneko/lua-language-server";
+    license = licenses.mit;
+    maintainers = with maintainers; [ mjlbach ];
+    platforms = platforms.linux;
+  };
+}
